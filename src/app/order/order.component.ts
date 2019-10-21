fd9de8eb5d6670c3cc55973f4ce64d44a76dc4be
Thiago Murakami <thiago_murakami@hotmail.com>
2019-10-21 20:34:15 -0300

    Update order.component.ts

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,8 +4,7 @@ import { CartService } from '../restaurant-detail/cart/cart.service';
 
 @Component({
   selector: 'app-order',
-  templateUrl: './order.component.html',
-  styleUrls: ['./order.component.css']
+  templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
 
