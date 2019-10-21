ce54dec4b37fc9b3bee42e84913dec706c980e0c
Thiago Murakami <thiago_murakami@hotmail.com>
2019-10-21 20:33:58 -0300

    Update notfound.component.ts

--- src/app/notfound/notfound.component.ts
+++ src/app/notfound/notfound.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-notfound',
-  templateUrl: './notfound.component.html',
-  styleUrls: ['./notfound.component.css']
+  templateUrl: './notfound.component.html'
 })
 export class NotfoundComponent implements OnInit {
 
